3894f983561d0ad94ff27cb0d1c230dd917bff6e
y0zz3r <117989718+y0zz3r@users.noreply.github.com>
2023-02-24 16:35:37 +0000

    Update bridge.sh

--- bridge.sh
+++ bridge.sh
@@ -1,22 +1,44 @@
 #!/bin/bash
 
-# Update the package list and upgrade installed packages
-sudo apt update
-sudo apt upgrade -y
+# Prompt user for bridge nickname
+read -p "Enter a nickname for your Tor bridge: " nickname
+
+# Generate random SSH port number between 1024 and 65535
+ssh_port=$(shuf -i 1024-65535 -n 1)
+
+# Update package repository and install necessary packages
+apt update
+apt install -y tor ufw
+
+# Configure firewall
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow $ssh_port/tcp
+ufw allow 9001/tcp
+ufw --force enable
+
+# Add Tor Project signing key and repository to package manager
+curl -fsSL https://deb.torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --dearmor | tee /usr/share/keyrings/torproject-archive-keyring.gpg >/dev/null 2>&1
+echo "deb [signed-by=/usr/share/keyrings/torproject-archive-keyring.gpg] https://deb.torproject.org/torproject.org $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/tor.list >/dev/null 2>&1
+apt update
 
 # Install Tor
-sudo apt install tor -y
+apt install -y tor
 
-# Backup the original Tor configuration file
-sudo cp /etc/tor/torrc /etc/tor/torrc.bak
+# Configure Tor bridge with user-provided nickname and default port for bridges (443)
+echo "BridgeRelay 1" >> /etc/tor/torrc
+echo "BridgeNickname $nickname" >> /etc/tor/torrc
+echo "ORPort 443" >> /etc/tor/torrc
+echo "ExitPolicy reject *:*" >> /etc/tor/torrc
 
-# Configure the Tor bridge settings
-echo "BridgeRelay 1" | sudo tee -a /etc/tor/torrc
-echo "ORPort 443" | sudo tee -a /etc/tor/torrc
-echo "Nickname <your_bridge_nickname>" | sudo tee -a /etc/tor/torrc
-echo "ContactInfo <your_email_address>" | sudo tee -a /etc/tor/torrc
-echo "ExitPolicy reject *:*" | sudo tee -a /etc/tor/torrc
-echo "Log notice file /var/log/tor/notices.log" | sudo tee -a /etc/tor/torrc
+# Change SSH port to random port number
+sed -i "s/^#Port 22/Port $ssh_port/" /etc/ssh/sshd_config
 
-# Restart Tor
-sudo systemctl restart tor
+# Restart SSH and Tor services
+systemctl restart sshd
+systemctl restart tor
+
+# Display Tor bridge fingerprint and bandwidth information, as well as the new SSH port number
+echo "Your Tor bridge fingerprint and bandwidth information:"
+cat /var/lib/tor/fingerprint
+echo "Your new SSH port number is $ssh_port"
